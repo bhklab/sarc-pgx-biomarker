71889b44f770e8a4296a6f1b47e8f4421f9b86b0
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-09-10 13:36:35 -0400

    prepare data for network analysis cytoscape

--- workflow/pathway/Prog_network_analysis.R
+++ workflow/pathway/Prog_network_analysis.R
@@ -33,6 +33,7 @@ library(qs)
 library(dplyr)
 library(readr)
 library(purrr)
+library(GSEABase)
 library(stringr)
 
 ##################################################################
@@ -43,28 +44,64 @@ dir_in <- 'data/results/drug'
 dir_pathway <- 'data/rawdata' 
 dir_out <- 'data/results' 
 
-thr_gsea <- 0.15
-###################################################################
-## load GO and HALLMARK pathways downloaded from msigdb
-##################################################################
+thr_gsea <- 0.001
+
+###################################################
+## Load pathway data
+###################################################
 
 hallmark_pathway <- gmtPathways(file.path(dir_pathway, "h.all.v2025.1.Hs.symbols.gmt"))
 go_pathway <- gmtPathways(file.path(dir_pathway, "c5.go.bp.v2025.1.Hs.symbols.gmt"))
 
-dat <- list(HALLMARK = hallmark_pathway, GO = go_pathway)
-qsave(dat, file=  file.path(dir_pathway, "pathway_data.qs"))
+###################################################
+## GSEA: GO:BP result
+###################################################
+# load selected drugs
+selected_drug <- read.csv(file.path('data/results/drug', 'selected_drugs.csv'))
+selected_drug_sts <- selected_drug[selected_drug$sts == 'Yes', 'drug']
+
+# load GSEA results using GO:BP 
+dat <- qread(file.path(dir_out, 'pathway', 'go_gsea_pathway_drug.qs'))
+dat <- dat[!is.na(dat$pval), ]
+dat$pathway_update <- tools::toTitleCase(tolower(substr(dat$pathway, 6, nchar(dat$pathway))))
+lab <- data.frame(
+  Name = dat$pathway,  # must match the EM node "name"
+  pretty_name = dat$pathway_update
+)
+write.table(lab, file = file.path(dir_out, 'network/GO', "labels.tsv"), sep="\t", quote=FALSE, row.names=FALSE)
+
+sig <- dat[dat$drug %in% selected_drug_sts & dat$padj < thr_gsea, ]
+
+drug <- unique(sig$drug)
+for(i in 1:length(drug)){
+
+df <-  sig[sig$drug == drug[i], c("pathway", "pval", "padj", "NES")]
+
+write.table(df, file = file.path(dir_out, 'network/GO/sig_0.001', paste(drug[i], 'gsea_go.txt', sep=".")), 
+            sep = "\t",
+            quote = FALSE,
+            row.names = FALSE)
+
+df <- dat[dat$drug == drug[i], c("pathway", "pval", "padj", "NES")]
+
+write.table(df, file = file.path(dir_out, 'network/GO/GSEA', paste(drug[i], 'gsea_go.txt', sep=".")), 
+            sep = "\t",
+            quote = FALSE,
+            row.names = FALSE)
+
+}
 
 ###################################################
 ## GSEA: STS gene association result and GO
 ###################################################
-# ---- 1) Ranked list (.rnk) ----
+# Ranked list (.rnk)
 dat <- qread(file= file.path(dir_in, "gene_drug_assoc_sts_meta.qs"))
 dat <- dat[!is.na(dat$r), ]
-drugs <- unique(dat$drug)
+drug <- intersect(unique(dat$drug), selected_drug_sts)
 
-for(i in 1:length(drugs)){
+for(i in 1:length(drug)){
 
-df <- dat[dat$drug == drugs[i], ] 
+df <- dat[dat$drug == drug[i], ] 
 ranks <- sign(df$r) * (-log10(df$pval))
 names(ranks) <- df$gene_name
 ranks <- sort(ranks, decreasing = T)
@@ -76,53 +113,9 @@ rnk <- tibble(
 ) %>%
   arrange(desc(Score))
 
-write_tsv(rnk,  file = file.path(dir_out, 'network/ranks', paste(drugs[i], 'metaCorRanks.rnk', sep='_')))  
+write_tsv(rnk,  file = file.path(dir_out, 'network/ranks', paste(drug[i], 'metaCorRanks.rnk', sep='_')))  
 
 }
-
-# ---- 2) Results file(s) in EM Generic format ----
-# Keep only significant pathways (adjust threshold as you like)
-
-fgseaRes <- qread(file.path(dir_out, 'pathway', "go_gsea_pathway_drug.qs"))
-fgseaRes$leadingEdge <- lapply(
-  fgseaRes$leadingEdge,
-  function(x) {
-    # x is a single "A,B,C" string now
-    genes <- unlist(strsplit(x, ","))        # split on comma
-    genes <- str_trim(genes)                 # trim whitespace
-    genes <- genes[genes != ""]              # drop empties
-    paste(unique(genes), collapse = "/")     # collapse with "/"
-  }
-)
-
-res_all <- fgseaRes %>%
-  mutate(
-    drug = drug,
-    Name   = pathway,
-    NES    = as.numeric(NES),
-    pvalue = as.numeric(pval),
-    qvalue = as.numeric(padj),
-    genes  = leadingEdge
-  )
-
-res_sig <- res_all 
-
-drugs <- unique(res_sig$drug)
-for(i in 1:length(drugs)){
-
- df <- res_sig[res_sig$drug == drugs[i], -1]
- df_pos <- res_sig[res_sig$drug == drugs[i] & res_sig$NES > 0, -1]
- df_neg <- res_sig[res_sig$drug == drugs[i] & res_sig$NES < 0, -1]
-
- write_tsv(df, file = file.path(dir_out, 'network/sig', paste(drugs[i], 'metaCor_gsea.tsv')))
- write_tsv(df_pos, file = file.path(dir_out, 'network/sig-pos', paste(drugs[i], 'metaCor_gsea_pos.tsv', sep='_')))
- write_tsv(df_neg, file = file.path(dir_out, 'network/sig-neg', paste(drugs[i], 'metaCor_gsea_neg.tsv', sep='_')))
-
-}
-
-
-
-
 
 
 
